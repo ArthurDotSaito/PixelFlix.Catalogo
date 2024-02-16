7f0fb7ee778ee00ddc04173e1568a305a0b71198
Arthur Saito <arthur.saito@nuria.com.br>
2024-02-16 16:02:28 -0300

    [feat] implementing basic Category Domain

--- src/FC.Pixelflix.Catalogo.Domain/Entities/Category.cs
+++ src/FC.Pixelflix.Catalogo.Domain/Entities/Category.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace FC.Pixelflix.Catalogo.Domain.Entities;
-internal class Category
+public class Category
 {
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public bool IsActive { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    public Category(string name, string description)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Description = description;
+        IsActive = true;
+        CreatedAt = DateTime.Now;  
+    }
 }
--- tests/FC.PixelFlix.Catalogo.UnitTests/Domain/Entities/Category/CategoryTest.cs
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Domain/Entities/Category/CategoryTest.cs
@@ -14,12 +14,18 @@ public class CategoryTest
             Name = "category name",
             Description = "category description"
         };
+        var dateTimeBefore = DateTime.Now;
 
         var category = new DomainEntity.Category(validData.Name, validData.Description);
+        var dateTimeAfter = DateTime.Now;
 
         Assert.NotNull(category);
         Assert.Equal(validData.Name, category.Name);
         Assert.Equal(validData.Description, category.Description);
-
+        Assert.NotEqual(default(Guid), category.Id);
+        Assert.NotEqual(default(DateTime), category.CreatedAt);
+        Assert.True(category.CreatedAt > dateTimeBefore);
+        Assert.True(category.CreatedAt < dateTimeAfter);
+        Assert.True(category.IsActive);
     }
 }
