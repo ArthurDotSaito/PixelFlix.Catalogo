c355bfbada821d0bdb6401b96a5bd54dbdc4a849
Arthur Saito <arthur.saito@nuria.com.br>
2024-02-16 15:20:58 -0300

    Adicionar arquivos de projeto.

---
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Domain/Entities/Category/CategoryTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace FC.PixelFlix.Catalogo.UnitTests.Domain.Entities.Category;
+public class CategoryTest
+{
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Category - Aggregates")]
+    public void Instantiate()
+    {
+        var validData = new
+        {
+            Name = "category name",
+            Description = "category description"
+        };
+
+        var category = new Category(validData.Name, validData.Description);
+
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+
+    }
+}
