0a3af6adcf329f4394a22c90713401470fe0581e
Arthur Saito <arthur.saito@nuria.com.br>
2024-02-19 13:28:21 -0300

    [feat] new DomainValidation notNull

---
+++ src/FC.Pixelflix.Catalogo.Domain/Validation/DomainValidation.cs
@@ -0,0 +1,14 @@
+﻿
+using FC.Pixelflix.Catalogo.Domain.Exceptions;
+
+namespace FC.Pixelflix.Catalogo.Domain.Validation;
+public class DomainValidation
+{
+    public static void NotNullDomainValidation(object target, string fieldName)
+    {
+        if(target == null)
+        {
+            throw new EntityValidationException($"{fieldName} should not be null");
+        }
+    }
+}
---
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -0,0 +1,32 @@
+﻿
+using Bogus;
+using FC.Pixelflix.Catalogo.Domain.Exceptions;
+using FC.Pixelflix.Catalogo.Domain.Validation;
+using FluentAssertions;
+using Xunit;
+
+namespace FC.PixelFlix.Catalogo.UnitTests.Domain.Validation;
+public class DomainValidationTest
+{
+    private Faker Faker { get; set; } = new Faker();
+
+    [Fact(DisplayName = nameof(givenANotNullDomainValidation_whenFieldIsNotNull_shouldBeOk))]
+    [Trait("Domain", "Domain Validation - Validation" )]
+    public void givenANotNullDomainValidation_whenFieldIsNotNull_shouldBeOk()
+    {
+        var aValidValue = Faker.Commerce.ProductName();
+        Action action = () => DomainValidation.NotNullDomainValidation(aValidValue, "Value");
+
+        action.Should().NotThrow();
+    }
+
+    [Fact(DisplayName = nameof(givenANotNullDomainValidation_whenFieldIsNull_shouldThrowsAnException))]
+    [Trait("Domain", "Domain Validation - Validation")]
+    public void givenANotNullDomainValidation_whenFieldIsNull_shouldThrowsAnException()
+    {
+        string nullField = null;
+        Action action = () => DomainValidation.NotNullDomainValidation(nullField, "FieldName");
+
+        action.Should().Throw<EntityValidationException>().WithMessage("FieldName should not be null");
+    }
+}
