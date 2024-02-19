38f9be4d2fed6d070b9e3c3d9f67a639d6f517e0
Arthur Saito <arthur.saito@nuria.com.br>
2024-02-18 23:55:30 -0300

    [chore]  implementing fuzzy data on tests

--- tests/FC.PixelFlix.Catalogo.UnitTests/Domain/Entities/Category/CategoryTestFixture.cs
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Domain/Entities/Category/CategoryTestFixture.cs
@@ -1,17 +1,20 @@
-﻿using Xunit;
+﻿using FC.PixelFlix.Catalogo.UnitTests.Common;
+using Xunit;
 using DomainEntity = FC.Pixelflix.Catalogo.Domain.Entities;
 
 
 namespace FC.PixelFlix.Catalogo.UnitTests.Domain.Entities.Category;
 
-public class CategoryTestFixture
+public class CategoryTestFixture : BaseFixture
 {
+
+    public CategoryTestFixture() : base() { }
     public DomainEntity.Category GetValidCategory()
     {
         var validData = new
         {
-            Name = "category name",
-            Description = "category description"
+            Name = Faker.Commerce.Categories(1)[0],
+            Description = Faker.Commerce.ProductDescription()
         };
 
         var category = new DomainEntity.Category(validData.Name, validData.Description, true);
