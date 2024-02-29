554f9aaed61c7fe2b47ce89385f0ae7eb7dac6a1
Arthur Saito <arthur.saito@nuria.com.br>
2024-02-29 18:10:10 -0300

    improving CreateCategory useCase test

--- tests/FC.PixelFlix.Catalogo.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -1,12 +1,20 @@
-﻿using Xunit;
+﻿using Moq;
+using Xunit;
+using useCases = FC.PixelFlix.Catalogo.Application.UseCases.CreateCategory;
 
 namespace FC.PixelFlix.Catalogo.UnitTests.Application.CreateCategory;
 public class CreateCategoryTest
 {
-    [Fact(DisplayName = nameof)]
+    [Fact(DisplayName = nameof()]
     [Trait("Application", "CreateCategory - Use Cases")]
-    public void CreateCategory()
+    public async void CreateCategory()
     {
+        var repositoryMock = new Mock<ICategoryRepository>();
+        var unitOfWork = new Mock<IUnitOfWork>();
+        var useCase = new useCases.CreateCategory(repositoryMock.Object, unitOfWork.Object);
 
+        var input = new CreateCategoryInput("categoryName", "categoryDescription", true);
+
+        var output = await useCase.Execute(input);
     }
 }
