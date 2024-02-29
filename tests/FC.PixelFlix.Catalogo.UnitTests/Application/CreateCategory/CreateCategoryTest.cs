04fec74dd3797cb9046052bd9c73b3650e3e0c88
Arthur Saito <arthur.saito@nuria.com.br>
2024-02-29 17:56:23 -0300

    [test] add CreateCategory UseCases with Moq

---
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace FC.PixelFlix.Catalogo.UnitTests.Application.CreateCategory;
+public class CreateCategoryTest
+{
+    [Fact(DisplayName = nameof)]
+    [Trait("Application", "CreateCategory - Use Cases")]
+    public void CreateCategory()
+    {
+
+    }
+}
