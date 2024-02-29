133eeede06913fce885a61557a0bdc30653f7bda
Arthur Saito <arthur.saito@nuria.com.br>
2024-02-29 18:20:14 -0300

    improving CreateCategory useCase test with cancellation token

--- tests/FC.PixelFlix.Catalogo.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using FC.Pixelflix.Catalogo.Domain.Entities;
+using Moq;
 using Xunit;
 using useCases = FC.PixelFlix.Catalogo.Application.UseCases.CreateCategory;
 
@@ -9,12 +10,29 @@ public class CreateCategoryTest
     [Trait("Application", "CreateCategory - Use Cases")]
     public async void CreateCategory()
     {
+        var expectedName = "categoryName";
+        var expectedDescription = "A category description";
+        var expectedIsActive = true;
+
         var repositoryMock = new Mock<ICategoryRepository>();
-        var unitOfWork = new Mock<IUnitOfWork>();
-        var useCase = new useCases.CreateCategory(repositoryMock.Object, unitOfWork.Object);
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var useCase = new useCases.CreateCategory(repositoryMock.Object, unitOfWorkMock.Object);
 
-        var input = new CreateCategoryInput("categoryName", "categoryDescription", true);
+        var input = new CreateCategoryInput(expectedName, expectedDescription, expectedIsActive);
 
-        var output = await useCase.Execute(input);
+        var output = await useCase.Execute(input, CancellationToken.None);
+
+        repositoryMock.Verify(repository => repository.Create(It.IsAny<Category>(), CancellationToken.None), Times.Once);
+        unitOfWorkMock.Verify(uow => uow.Commit(),Times.Once);
+
+        output.Should().NotBeNull();
+        output.Id.Should().NotBeNull();
+        output.Id.Should().NotBeEmpty();
+        output.Id.Should().NotBe(default(Guid));
+        output.Name.Should().BeEqual(expectedName);
+        output.Description.Should().BeEqual(expectedDescription);
+        output.IsActive.Should().BeTrue();
+        output.CreatedAt.Should().NotBeNull();
+        output.CreatedAt.Should().NotBe(default(DateTime));
     }
 }
