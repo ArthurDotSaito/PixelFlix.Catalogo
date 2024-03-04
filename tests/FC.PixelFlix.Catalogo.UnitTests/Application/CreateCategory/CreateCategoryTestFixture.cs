a9024774d21c4f7b18a1d115840592bc6906a372
Arthur Saito <arthur.saito@nuria.com.br>
2024-03-04 19:12:54 -0300

    [chore] add fixture in CreateCategory

---
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/CreateCategory/CreateCategoryTestFixture.cs
@@ -0,0 +1,39 @@
+﻿using FC.PixelFlix.Catalogo.UnitTests.Common;
+using FC.PixelFlix.Catalogo.UnitTests.Domain.Entities.Category;
+using Xunit;
+
+namespace FC.PixelFlix.Catalogo.UnitTests.Application.CreateCategory;
+
+[CollectionDefinition(nameof(CreateCategoryTestFixture))]
+public class CreateCategoryTestFixtureCollection : ICollectionFixture<CreateCategoryTestFixture> { }
+
+public class CreateCategoryTestFixture : BaseFixture
+{
+    public CreateCategoryTestFixture() : base() { }
+
+    public string GetValidCategoryName()
+    {
+        var aCategoryName = "";
+        while (aCategoryName.Length < 3)
+        {
+            aCategoryName = Faker.Commerce.Categories(1)[0];
+        }
+        if (aCategoryName.Length > 255)
+        {
+            aCategoryName = aCategoryName[..254];
+        }
+
+        return aCategoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var aCategoryDescription = Faker.Commerce.ProductDescription();
+        if (aCategoryDescription.Length > 10000)
+        {
+            aCategoryDescription = aCategoryDescription[..10000];
+        }
+
+        return aCategoryDescription;
+    }
+}
