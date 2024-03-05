7865531c6046ed3038b5a69afb544675b7bc03a0
Arthur Saito <arthur.saito@nuria.com.br>
2024-03-04 23:29:43 -0300

    [test] when create a category w/o description, should be OK

--- tests/FC.PixelFlix.Catalogo.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -18,9 +18,9 @@ public class CreateCategoryTest
         _fixture = fixture;
     }
 
-    [Fact(DisplayName = nameof(GivenAValidCommand_whenCallsCreateCategory_shouldReturnACategory))]
+    [Fact(DisplayName = nameof(GivenAValidCommand_whenCallsCreateCategory_shouldBeOk))]
     [Trait("Application", "CreateCategory - Use Cases")]
-    public async void GivenAValidCommand_whenCallsCreateCategory_shouldReturnACategory()
+    public async void GivenAValidCommand_whenCallsCreateCategory_shouldBeOk()
     {
         var repositoryMock = _fixture.GetMockRepository();
         var unitOfWorkMock = _fixture.GetMockUnitOfWork();
@@ -116,4 +116,37 @@ public class CreateCategoryTest
 
         return invalidInputList;
     }
+
+    [Fact(DisplayName = nameof(GivenAInvalidCommandWitName_whenCallsCreateCategory_shouldBeOk))]
+    [Trait("Application", "CreateCategory - Use Cases")]
+    public async void GivenAInvalidCommandWitName_whenCallsCreateCategory_shouldBeOk()
+    {
+        var repositoryMock = _fixture.GetMockRepository();
+        var unitOfWorkMock = _fixture.GetMockUnitOfWork();
+
+        var useCase = new useCases.CreateCategory(unitOfWorkMock.Object, repositoryMock.Object);
+
+        var input = new CreateCategoryInput(_fixture.GetValidCategoryName());
+
+        var output = await useCase.Execute(input, CancellationToken.None);
+
+        repositoryMock.Verify(repository =>
+            repository.Insert(It.IsAny<Category>(), It.IsAny<CancellationToken>()),
+            Times.Once
+            );
+
+        unitOfWorkMock.Verify(uow =>
+            uow.Commit(It.IsAny<CancellationToken>()), Times.Once
+            );
+
+        output.Should().NotBeNull();
+        output.Id.Should().NotBeEmpty();
+        output.Id.Should().NotBe(default(Guid));
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be("");
+        output.IsActive.Should().Be(input.IsActive);
+        output.CreatedAt.Should().NotBe(null);
+        output.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
+    }
+
 }
