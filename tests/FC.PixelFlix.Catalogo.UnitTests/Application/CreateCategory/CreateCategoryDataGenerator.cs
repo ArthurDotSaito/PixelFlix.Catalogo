59e7aa234ecb3ad8097a99ee8ea2349c4c94424f
Arthur Saito <arthur.saito@nuria.com.br>
2024-03-07 19:11:24 -0300

    [refact] refactoring DatabaseGen

--- tests/FC.PixelFlix.Catalogo.UnitTests/Application/CreateCategory/CreateCategoryDataGenerator.cs
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/CreateCategory/CreateCategoryDataGenerator.cs
@@ -1,10 +1,26 @@
 ﻿namespace FC.PixelFlix.Catalogo.UnitTests.Application.CreateCategory;
 public class CreateCategoryDataGenerator
 {
-    public static IEnumerable<object[]> GetInvalidInput()
+    public static IEnumerable<object[]> GetInvalidInput(int times = 12)
     {
         var fixture = new CreateCategoryTestFixture();
         var invalidInputList = new List<object[]>();
+        var totalInvalidCases = 4;
+
+        for (int i = 0;i < times; i++)
+        {
+            switch (i % totalInvalidCases)
+            {
+                case 0:
+                    invalidInputList.Add(new object[]
+                    {
+                        fixture.GetInvalidShortNameInput(),
+                        "Name should be at least 3 characters long"
+                    });
+                break;
+
+            }
+        }
 
         invalidInputList.Add(new object[]
         {
