6bbb01b1fb0a9b23b32562b23ec7c012b1e4527a
Arthur Saito <arthur.saito@nuria.com.br>
2024-03-09 15:53:08 -0300

    [test] add a basic validationRequest test on getCategory

---
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/GetCategory/GetCategoryRequestTest.cs
@@ -0,0 +1,35 @@
+﻿using FC.Pixelflix.Catalogo.Application.UseCases.Category.GetCategory.Dto;
+using Xunit;
+
+namespace FC.PixelFlix.Catalogo.UnitTests.Application.GetCategory;
+
+[Collection(nameof(GetCategoryTestFixture))]
+public class GetCategoryRequestTest
+{
+    private readonly GetCategoryTestFixture _fixture;
+
+    public GetCategoryRequestTest(GetCategoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName ="")]
+    [Trait("Application", "GetCategoryRequestTest - UseCases")]
+    public void GivenValidId_whenCallsGetCategory_shouldValidateOk()
+    {
+        //given
+        var aValidId = Guid.NewGuid();
+        var aValidRequest = new GetCategoryRequest(aValidId);
+
+        var validation = new GetCategoryRequestValidation();
+
+        //when
+        var aValidResult = validation.Validate(aValidRequest);
+
+        //then
+        aValidResult.Should().NotBeNull();  
+        aValidResult.IsValid.Should().BeTrue();
+        aValidResult.Error.Should().HaveCount(0);
+
+    }
+}
