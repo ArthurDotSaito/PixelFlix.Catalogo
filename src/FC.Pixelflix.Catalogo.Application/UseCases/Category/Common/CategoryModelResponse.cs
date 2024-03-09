7a1b5e31be47735182b2a940c1dc7add0bd95ecd
Arthur Saito <arthur.saito@nuria.com.br>
2024-03-08 23:33:02 -0300

    [chore] creating a common response class

---
+++ src/FC.Pixelflix.Catalogo.Application/UseCases/Category/Common/CategoryModelResponse.cs
@@ -0,0 +1,23 @@
+﻿using FC.Pixelflix.Catalogo.Application.UseCases.Category.CreateCategory.Dto;
+
+namespace FC.Pixelflix.Catalogo.Application.UseCases.Category.Common;
+public class CategoryModelResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public CategoryModelResponse(Guid id, string name, string description, bool isActive, DateTime createdAt)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        IsActive = isActive;
+        CreatedAt = createdAt;
+    }
+
+    public static CategoryModelResponse FromCategory(DomainEntity.Category aCategory) =>
+         new CategoryModelResponse(aCategory.Id, aCategory.Name, aCategory.Description, aCategory.IsActive, aCategory.CreatedAt);
+}
