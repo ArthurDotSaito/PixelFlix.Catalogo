3b948c5faace698e9f484ddc515c3c4fa151f1d6
Arthur Saito <arthur.saito@nuria.com.br>
2024-03-08 23:57:14 -0300

    [feat] add exceptions

---
+++ src/FC.Pixelflix.Catalogo.Application/Exceptions/ApplicationException.cs
@@ -0,0 +1,5 @@
+﻿namespace FC.Pixelflix.Catalogo.Application.Exceptions;
+public abstract class ApplicationException : Exception
+{
+    protected ApplicationException(string? message): base(message) { }
+}
---
+++ src/FC.Pixelflix.Catalogo.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace FC.Pixelflix.Catalogo.Application.Exceptions;
+public class NotFoundException : ApplicationException
+{
+    public NotFoundException(string? message) : base(message)
+    {
+    }
+}
