fe35c4e599bf5d86d7c0876b486f6141be858ef1
Arthur Saito <arthur.saito@nuria.com.br>
2024-03-11 18:36:06 -0300

    [test] add a test which verifies a positive case in DeleteCategory

--- tests/FC.PixelFlix.Catalogo.UnitTests/Application/DeleteCategory/DeleteCategoryTest.cs
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/DeleteCategory/DeleteCategoryTest.cs
@@ -1,14 +1,43 @@
-﻿using Xunit;
+﻿using Moq;
+using Xunit;
 
 namespace FC.PixelFlix.Catalogo.UnitTests.Application.DeleteCategory;
 
 [Collection(nameof(DeleteCategoryTestFixture))]
 internal class DeleteCategoryTest
-{
+{s
     private readonly DeleteCategoryTestFixture _fixture;
 
     public DeleteCategoryTest(DeleteCategoryTestFixture fixture)
     {
         _fixture = fixture;
+    }
+
+    [Fact(DisplayName ="")]
+    [Trait("Appliation", "DeleteCategory - Use Cases")]
+    public async Task GivenAValidId_whenCallsDeleteCategory_shouldBeOk()
+    {
+        //given
+        var aRepository = _fixture.GetRepositoryMock();
+        var aUnitOfWork = _fixture.GetMockUnitOfWork();
+
+        var aValidCategory = _fixture.GetValidCategory();
+
+        aRepository.Setup(category => category.Get(aValidCategory.Id, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(aValidCategory);
+
+        var request = new DeleteCategoryRequest(aValidCategory.Id);
+
+        var useCase = new DeleteCategory(aRepository.Object, aUnitOfWork.Object);
+        
+        //when
+        await useCase.Handle(request, CancellationToken.None);
+
+        //then
+        aRepository.Verify(category => category.Get(aValidCategory.Id, It.IsAny<CancellationToken>()), Times.Once);
+        aRepository.Verify(category => category.Delete(aValidCategory, It.IsAny<CancellationToken>()), Times.Once);
+        aUnitOfWork.Verify(category => category.Commit(It.IsAny<CancellationToken>()), Times.Once);
+
+
     }
 }
