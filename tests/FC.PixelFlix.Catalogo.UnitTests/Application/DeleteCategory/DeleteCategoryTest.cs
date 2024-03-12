db061af2d87363f1be78691d81f0eb8765fd0cc0
Arthur Saito <arthur.saito@nuria.com.br>
2024-03-11 23:42:04 -0300

    [test] add a test which should verify if DeleteCategory return a exception when null

--- tests/FC.PixelFlix.Catalogo.UnitTests/Application/DeleteCategory/DeleteCategoryTest.cs
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/DeleteCategory/DeleteCategoryTest.cs
@@ -2,6 +2,8 @@
 using Moq;
 using Xunit;
 using FC.Pixelflix.Catalogo.Application.UseCases.Category.DeleteCategory;
+using FC.Pixelflix.Catalogo.Application.Exceptions;
+using FluentAssertions;
 
 namespace FC.PixelFlix.Catalogo.UnitTests.Application.DeleteCategory;
 
@@ -15,7 +17,7 @@ public class DeleteCategoryTest
         _fixture = fixture;
     }
 
-    [Fact(DisplayName ="")]
+    [Fact(DisplayName = nameof(GivenAValidId_whenCallsDeleteCategory_shouldBeOk))]
     [Trait("Appliation", "DeleteCategory - Use Cases")]
     public async Task GivenAValidId_whenCallsDeleteCategory_shouldBeOk()
     {
@@ -39,7 +41,30 @@ public class DeleteCategoryTest
         aRepository.Verify(category => category.Get(aValidCategory.Id, It.IsAny<CancellationToken>()), Times.Once);
         aRepository.Verify(category => category.Delete(aValidCategory, It.IsAny<CancellationToken>()), Times.Once);
         aUnitOfWork.Verify(category => category.Commit(It.IsAny<CancellationToken>()), Times.Once);
+    }
 
+    [Fact(DisplayName = nameof(GivenAInexistentCategory_whenCallsDeleteCategory_shouldThrowNotFound))]
+    [Trait("Appliation", "DeleteCategory - Use Cases")]
+    public async Task GivenAInexistentCategory_whenCallsDeleteCategory_shouldThrowNotFound()
+    {
+        //given
+        var aRepository = _fixture.GetRepositoryMock();
+        var aUnitOfWork = _fixture.GetMockUnitOfWork();
 
+        var aInvalidCategory = _fixture.GetValidCategory();
+
+        aRepository.Setup(category => category.Get(aInvalidCategory.Id, It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new NotFoundException($"Category '{aInvalidCategory.Id} not found"));
+
+        var request = new DeleteCategoryRequest(aInvalidCategory.Id);
+
+        var useCase = new UseCases.DeleteCategory(aRepository.Object, aUnitOfWork.Object);
+
+        //when
+        var aTask = async() => await useCase.Handle(request, CancellationToken.None);
+
+        //then
+        await aTask.Should().ThrowAsync<NotFoundException>();
+        aRepository.Verify(category => category.Get(aInvalidCategory.Id, It.IsAny<CancellationToken>()), Times.Once);
     }
 }
