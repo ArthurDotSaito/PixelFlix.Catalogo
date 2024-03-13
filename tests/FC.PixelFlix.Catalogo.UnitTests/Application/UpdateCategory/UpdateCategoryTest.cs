10fd03c459b2de83f1db87c40968c1054f17a0f8
Arthur Saito <arthur.saito@nuria.com.br>
2024-03-13 17:53:38 -0300

    [chore] add UpdateCategoryTest Fixture

---
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/UpdateCategory/UpdateCategoryTest.cs
@@ -0,0 +1,22 @@
+﻿
+using Xunit;
+
+namespace FC.PixelFlix.Catalogo.UnitTests.Application.UpdateCategory;
+
+[Collection(nameof(UpdateCategoryTestCollection))]
+public class UpdateCategoryTest
+{
+    private readonly UpdateCategoryTestFixture _fixture;
+
+    public UpdateCategoryTest(UpdateCategoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName ="")]
+    [Trait("Application", "UpdateCategory - UseCases")]
+    public void GivenAValidId_whenCallsUpdateCategory_shouldReturnACategory()
+    {
+
+    }
+}
---
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/UpdateCategory/UpdateCategoryTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using FC.PixelFlix.Catalogo.UnitTests.Common;
+using Xunit;
+
+namespace FC.PixelFlix.Catalogo.UnitTests.Application.UpdateCategory;
+
+[CollectionDefinition(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryTestCollection : ICollectionFixture<UpdateCategoryTestFixture>{ }
+
+public class UpdateCategoryTestFixture : BaseFixture
+{
+}
