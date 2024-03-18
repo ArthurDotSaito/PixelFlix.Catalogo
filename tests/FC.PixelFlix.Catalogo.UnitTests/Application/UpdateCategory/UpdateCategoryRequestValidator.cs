bdcae347a587f33666552bbfcad272fd2fc2c941
Arthur Saito <arthur.saito@nuria.com.br>
2024-03-18 18:07:45 -0300

    [feat] add UpdateCategory Validator test

---
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/UpdateCategory/UpdateCategoryRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace FC.PixelFlix.Catalogo.UnitTests.Application.UpdateCategory;
+[Collection(nameof(UpdateCategoryTestCollection))]
+public class UpdateCategoryRequestValidator
+{
+    private readonly UpdateCategoryTestFixture _fixture;
+
+    public UpdateCategoryRequestValidator(UpdateCategoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+
+}
