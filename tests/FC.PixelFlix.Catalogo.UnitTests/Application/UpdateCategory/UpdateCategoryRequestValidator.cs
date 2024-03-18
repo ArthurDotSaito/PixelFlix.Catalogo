6025b072a8763acb7a5aa7638ad942d8e5f3a44e
Arthur Saito <arthur.saito@nuria.com.br>
2024-03-18 18:12:54 -0300

    UpdateCategory Id must not be empty

--- tests/FC.PixelFlix.Catalogo.UnitTests/Application/UpdateCategory/UpdateCategoryRequestValidator.cs
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/UpdateCategory/UpdateCategoryRequestValidator.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Xunit;
 
 namespace FC.PixelFlix.Catalogo.UnitTests.Application.UpdateCategory;
 [Collection(nameof(UpdateCategoryTestCollection))]
@@ -11,5 +12,24 @@ public class UpdateCategoryRequestValidator
         _fixture = fixture;
     }
 
+    [Fact(DisplayName = nameof(givenAEmptyGuid_whenCallsUpdateCategory_thenReturnAException))]
+    [Trait("Application", "UpdateCategoryTestValidator - UseCases")]
 
+    public void givenAEmptyGuid_whenCallsUpdateCategory_thenReturnAException()
+    {
+        //given
+        var expectedErrorMessage = "'Id' should not be empty";
+        var request = _fixture.GetValidRequest(Guid.Empty);
+
+        var validator = new UpdateCategoryRequestValidator();
+
+        //when
+        var validationResponse = validator.Validate(request);
+
+        //then
+        validationResponse.Should().NotBeNull();
+        validationResponse.IsValid.Should().BeFalse();
+        validationResponse.Errors.Should().HaveCount(1);
+        validationResponse.Errors[0].ErrorMessage.Should().Be(expectedErrorMessage);
+    }
 }
