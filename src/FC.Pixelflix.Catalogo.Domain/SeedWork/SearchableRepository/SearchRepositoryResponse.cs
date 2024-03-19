644605558aa027ed432af0480b22ef4a3d2f95c3
Arthur Saito <arthur.saito@nuria.com.br>
2024-03-19 19:53:38 -0300

    [chore] + SearchableRepo

---
+++ src/FC.Pixelflix.Catalogo.Domain/SeedWork/SearchableRepository/SearchRepositoryResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace FC.Pixelflix.Catalogo.Domain.SeedWork.SearchableRepository;
+public class SearchRepositoryResponse<TAggregate> where TAggregate : AggretateRoot
+{
+    public int CurrentPage { get; set; }
+    public int PerPage { get; set; }
+    public int Total { get; set; }
+    public IReadOnlyList<TAggregate> Items { get; set;}
+
+    public SearchRepositoryResponse(int currentPage, int perPage, int total, IReadOnlyList<TAggregate> items)
+    {
+        CurrentPage = currentPage;
+        PerPage = perPage;
+        Total = total;
+        Items = items;
+    }
+
+}
