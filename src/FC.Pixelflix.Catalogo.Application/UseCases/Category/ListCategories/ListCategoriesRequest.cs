49ce304cce757688691d612f98af66c30c36bcd5
Arthur Saito <arthur.saito@nuria.com.br>
2024-03-19 20:50:14 -0300

    add ListCategories response & request

---
+++ src/FC.Pixelflix.Catalogo.Application/UseCases/Category/ListCategories/ListCategoriesRequest.cs
@@ -0,0 +1,9 @@
+﻿using FC.Pixelflix.Catalogo.Application.Common;
+using FC.Pixelflix.Catalogo.Domain.SeedWork.SearchableRepository;
+using MediatR;
+
+namespace FC.Pixelflix.Catalogo.Application.UseCases.Category.ListCategories;
+public class ListCategoriesRequest : PaginatedListRequest, IRequest
+{
+    public ListCategoriesRequest(int page, int perPage, string search, string sort, SearchOrder dir) : base(page, perPage, search, sort, dir){}
+}
---
+++ src/FC.Pixelflix.Catalogo.Application/UseCases/Category/ListCategories/ListCategoriesResponse.cs
@@ -0,0 +1,8 @@
+﻿using FC.Pixelflix.Catalogo.Application.Common;
+using FC.Pixelflix.Catalogo.Application.UseCases.Category.Common;
+
+namespace FC.Pixelflix.Catalogo.Application.UseCases.Category.ListCategories;
+public class ListCategoriesResponse : PaginatedListResponse<CategoryModelResponse>
+{
+    public ListCategoriesResponse(int page, int perPage, int total, IReadOnlyList<CategoryModelResponse> items) : base(page, perPage, total, items){}
+}
