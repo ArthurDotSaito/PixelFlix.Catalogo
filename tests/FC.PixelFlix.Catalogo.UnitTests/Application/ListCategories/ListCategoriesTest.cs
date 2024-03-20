0071f7144dd778e2c56cbcdfea24147396562e29
Arthur Saito <arthur.saito@nuria.com.br>
2024-03-20 20:06:28 -0300

    [chore] add random auto request data at requests in ListCategories test

--- tests/FC.PixelFlix.Catalogo.UnitTests/Application/ListCategories/ListCategoriesTest.cs
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/ListCategories/ListCategoriesTest.cs
@@ -5,6 +5,7 @@ using FluentAssertions;
 using Moq;
 using Xunit;
 using FC.Pixelflix.Catalogo.Application.UseCases.Category.Common;
+using FC.Pixelflix.Catalogo.Application.UseCases.Category.ListCategories;
 
 namespace FC.PixelFlix.Catalogo.UnitTests.Application.ListCategories;
 
@@ -31,7 +32,72 @@ public class ListCategoriesTest
                     currentPage: request.Page,
                     perPage: request.PerPage,
                     items: (IReadOnlyList<Category>)aCategoryList,
-                    total: (new Random()).Next(50,200));
+                    total: (new Random()).Next(50,200)
+        );
+
+        aRepository.Setup(category => category.Search(
+            It.Is<SearchRepositoryRequest>(searchRequest =>
+                    searchRequest.Page == request.Page &&
+                    searchRequest.PerPage == request.PerPage &&
+                    searchRequest.Search == request.Search &&
+                    searchRequest.OrderBy == request.Sort &&
+                    searchRequest.Order == request.Dir
+                ),
+            It.IsAny<CancellationToken>())
+        ).ReturnsAsync(repositoryResponse);
+
+        var useCase = new UseCase.ListCategories(aRepository.Object);
+
+        //when
+        var response = await useCase.Handle(request, CancellationToken.None);
+
+        //then
+        response.Should().NotBeNull();
+        response.Page.Should().Be(repositoryResponse.CurrentPage);
+        response.PerPage.Should().Be(repositoryResponse.PerPage);
+        response.Total.Should().Be(repositoryResponse.Total);
+        response.Items.Should().HaveCount(repositoryResponse.Items.Count);
+        ((List<CategoryModelResponse>)response.Items).ForEach(item =>
+        {
+            var aCategory = repositoryResponse.Items.FirstOrDefault(a => a.Id == item.Id);
+            item.Should().NotBeNull();
+            item.Name.Should().Be(aCategory.Name);
+            item.Description.Should().Be(aCategory.Description);
+            item.IsActive.Should().Be(aCategory.IsActive);
+            item.CreatedAt.Should().Be(aCategory.CreatedAt);
+        });
+
+        aRepository.Verify(category => category.Search(
+            It.Is<SearchRepositoryRequest>(searchRequest =>
+                    searchRequest.Page == request.Page &&
+                    searchRequest.PerPage == request.PerPage &&
+                    searchRequest.Search == request.Search &&
+                    searchRequest.OrderBy == request.Sort &&
+                    searchRequest.Order == request.Dir
+                ),
+            It.IsAny<CancellationToken>()
+            ));
+    }
+
+    [Theory(DisplayName = nameof(GivenAValidDefaultRequest_WhenCallsListCategories_ShouldReturnOk))]
+    [Trait("Application", "ListCategories - UseCases")]
+    [MemberData(
+        nameof(ListCategoryDataGenerator.GetRequestWithDefaultParams),
+        parameters:12,
+        MemberType = typeof(ListCategoryDataGenerator)
+    )]
+    public async Task GivenAValidDefaultRequest_WhenCallsListCategories_ShouldReturnOk(ListCategoriesRequest request)
+    {
+        //given
+        var aCategoryList = _fixture.GetValidCategoryList();
+        var aRepository = _fixture.GetRepositoryMock();
+
+        var repositoryResponse = new SearchRepositoryResponse<Category>(
+                    currentPage: request.Page,
+                    perPage: request.PerPage,
+                    items: (IReadOnlyList<Category>)aCategoryList,
+                    total: (new Random()).Next(50, 200)
+        );
 
         aRepository.Setup(category => category.Search(
             It.Is<SearchRepositoryRequest>(searchRequest =>
---
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/ListCategories/ListCategoryDataGenerator.cs
@@ -0,0 +1,40 @@
+﻿using FC.Pixelflix.Catalogo.Application.UseCases.Category.ListCategories;
+
+namespace FC.PixelFlix.Catalogo.UnitTests.Application.ListCategories;
+public class ListCategoryDataGenerator
+{
+    public static IEnumerable<object[]> GetRequestWithDefaultParams(int times = 12)
+    {
+        var fixture = new ListCategoriesTestFixture();
+
+        var aRequest = fixture.GetValidRequest();
+
+        for(int i = 0; i < times; i++)
+        {
+            switch (i % 6)
+            {
+                case 0:
+                    yield return new object[] {new ListCategoriesRequest()};
+                    break;
+                case 1:
+                    yield return new object[] { new ListCategoriesRequest(aRequest.Page) };
+                    break;
+                case 2:
+                    yield return new object[] { new ListCategoriesRequest(aRequest.Page, aRequest.PerPage) };
+                    break;
+                case 3:
+                    yield return new object[] { new ListCategoriesRequest(aRequest.Page, aRequest.PerPage, aRequest.Search) };
+                    break;
+                case 4:
+                    yield return new object[] { new ListCategoriesRequest(aRequest.Page, aRequest.PerPage, aRequest.Search, aRequest.Sort) };
+                    break;
+                case 5:
+                    yield return new object[] { new ListCategoriesRequest(aRequest) };
+                    break;
+                default:
+                    yield return new object[] { new ListCategoriesRequest() };
+                    break;
+            }
+        }
+    }
+}
