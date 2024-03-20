61d486b1c81fa6cc7108a9123ff740b1130cf69c
Arthur Saito <arthur.saito@nuria.com.br>
2024-03-19 23:03:31 -0300

    [chore] add Handle method

---
+++ src/FC.Pixelflix.Catalogo.Application/UseCases/Category/ListCategories/ListCategories.cs
@@ -0,0 +1,27 @@
+﻿
+using FC.Pixelflix.Catalogo.Application.UseCases.Category.Common;
+using FC.Pixelflix.Catalogo.Domain.Repository;
+
+namespace FC.Pixelflix.Catalogo.Application.UseCases.Category.ListCategories;
+public class ListCategories : IListCategories
+{
+    private readonly ICategoryRepository _categoryRepository;
+
+    public ListCategories(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<ListCategoriesResponse> Handle(ListCategoriesRequest request, CancellationToken cancellationToken)
+    {
+        var repositoryListResponse = await _categoryRepository.Search(new(request.Page, request.PerPage, request.Search, request.Sort, request.Dir), 
+            cancellationToken);
+
+        return new ListCategoriesResponse(
+            repositoryListResponse.CurrentPage,
+            repositoryListResponse.PerPage,
+            repositoryListResponse.Total,
+            repositoryListResponse.Items.Select(CategoryModelResponse.FromCategory).ToList());
+
+    }
+}
