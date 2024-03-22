ff3392038998dd4f2d3dda4d60c2569a62c07d93
Arthur Saito <arthur.saito@nuria.com.br>
2024-03-22 14:45:43 -0300

    [chore] config context to Categories

--- src/FC.Pixelflix.Catalogo.Infra.Data.EF/Configurations/CategoryConfiguration.cs
+++ src/FC.Pixelflix.Catalogo.Infra.Data.EF/Configurations/CategoryConfiguration.cs
@@ -1,4 +1,14 @@
-﻿namespace FC.Pixelflix.Catalogo.Infra.Data.EF.Configurations;
-public class CategoryConfiguration
+﻿using FC.Pixelflix.Catalogo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FC.Pixelflix.Catalogo.Infra.Data.EF.Configurations;
+internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
 {
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasKey(category => category.Id);
+        builder.Property(category => category.Name).HasMaxLength(255);
+        builder.Property(category => category.Description).HasMaxLength(10000);
+    }
 }
---
+++ src/FC.Pixelflix.Catalogo.Infra.Data.EF/PixelflixCatalogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using FC.Pixelflix.Catalogo.Domain.Entities;
+using FC.Pixelflix.Catalogo.Infra.Data.EF.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace FC.Pixelflix.Catalogo.Infra.Data.EF;
+internal class PixelflixCatalogDbContext : DbContext
+{
+    public DbSet<Category> Categories => Set<Category>();
+
+    public PixelflixCatalogDbContext(DbContextOptions<PixelflixCatalogDbContext> options) : base(options){}
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new CategoryConfiguration());    
+    }
+
+}
