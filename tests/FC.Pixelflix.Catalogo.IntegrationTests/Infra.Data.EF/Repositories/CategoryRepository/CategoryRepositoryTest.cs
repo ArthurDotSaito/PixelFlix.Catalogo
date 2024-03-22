7851d7a39f40cfa4c24c227af2ea07fde17a7a3f
Arthur Saito <arthur.saito@nuria.com.br>
2024-03-22 14:32:05 -0300

    [chore] add basic test and fixtures to IT test

--- tests/FC.Pixelflix.Catalogo.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTest.cs
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTest.cs
@@ -1,8 +1,37 @@
-﻿using Xunit;
+﻿using Microsoft.EntityFrameworkCore;
+using FluentAssertions;
+using Xunit;
 
 namespace FC.Pixelflix.Catalogo.IntegrationTests.Infra.Data.EF.Repositories.CategoryRepository;
 
 [Collection(nameof(CategoryRepositoryTestFixtureCollection))]
 public class CategoryRepositoryTest
 {
+    private readonly CategoryRepositoryTestFixture _fixture;
+
+    public CategoryRepositoryTest(CategoryRepositoryTestFixture categoryRepositoryTestFixture)
+    {
+        _fixture = categoryRepositoryTestFixture;
+    }
+
+    [Fact(DisplayName = "Verificar categoria e propriedades no repositório")]
+    [Trait("Integration/Infra.Data", "CategoryRepository - Repositories")]
+    public async Task givenAValidCategory_whenCallsInsert_shouldBeOk()
+    {
+        PixelFlixCatalogDbContext dbContext = _fixture.CreateDbContext();
+        var aCategory = _fixture.GetValidCategory();
+
+        var aCategoryRepository = new CategoryRepository(dbContext);
+
+        await aCategoryRepository.Insert(aCategory, CancellationToken.None);
+        await dbContext.SaveChangesAsync();
+
+        var dbCategory = await dbContext.Categories.Find(aCategory.Id);
+
+        dbCategory.Should().NotBeNull();
+        dbCategory.Name.Should().Be(aCategory.Name);
+        dbCategory.Description.Should().Be(aCategory.Description);
+        dbCategory.IsActive.Should().Be(aCategory.IsActive);
+        dbCategory.CreatedAt.Should().Be(aCategory.CreatedAt);
+    }
 }
--- tests/FC.Pixelflix.Catalogo.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTestFixture.cs
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTestFixture.cs
@@ -1,12 +1,65 @@
-﻿using FC.Pixelflix.Catalogo.IntegrationTests.Base;
+﻿using FC.Pixelflix.Catalogo.Domain.Entities;
+using FC.Pixelflix.Catalogo.IntegrationTests.Base;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace FC.Pixelflix.Catalogo.IntegrationTests.Infra.Data.EF.Repositories.CategoryRepository;
 
 [CollectionDefinition(nameof(CategoryRepositoryTestFixtureCollection))]
 public class CategoryRepositoryTestFixtureCollection : ICollectionFixture<CategoryRepositoryTestFixture> { }
-
+    
 public class CategoryRepositoryTestFixture : BaseFixture
 {
+    public PixelFlixCatalogDbContext CreateDbContext()
+    {
+        var dbContext = new PixelFlixCatalogDbContext(
+            new DbContextOptionsBuilder<PixelFlixCatalogDbContext>()
+            .UseInMemoryDatabase("integration-tests-db")
+            .Options
+        );
+    }
 
+    public string GetValidCategoryName()
+    {
+        var aCategoryName = "";
+        while (aCategoryName.Length < 3)
+        {
+            aCategoryName = Faker.Commerce.Categories(1)[0];
+        }
+        if (aCategoryName.Length > 255)
+        {
+            aCategoryName = aCategoryName[..254];
+        }
+
+        return aCategoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var aCategoryDescription = Faker.Commerce.ProductDescription();
+        if (aCategoryDescription.Length > 10000)
+        {
+            aCategoryDescription = aCategoryDescription[..10000];
+        }
+
+        return aCategoryDescription;
+    }
+
+    public bool GetRandomIsActive()
+    {
+        return new Random().NextDouble() < 0.5;
+    }
+
+    public Category GetValidCategory()
+    {
+        var validData = new
+        {
+            Name = GetValidCategoryName(),
+            Description = GetValidCategoryDescription(),
+            IsActive = GetRandomIsActive()
+        };
+
+        var category = new Category(validData.Name, validData.Description, validData.IsActive);
+        return category;
+    }
 }
