8589b0af08dd4ace19a8515c51ded9e5e5356ae4
Arthur Saito <arthur.saito@nuria.com.br>
2024-04-26 22:35:56 -0300

    [feat] implementing test to verify search behaviour, returning total items

--- tests/FC.Pixelflix.Catalogo.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTest.cs
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTest.cs
@@ -3,6 +3,8 @@ using Xunit;
 using FC.Pixelflix.Catalogo.Infra.Data.EF;
 using Repository = FC.Pixelflix.Catalogo.Infra.Data.EF.Repositories;
 using FC.Pixelflix.Catalogo.Application.Exceptions;
+using FC.Pixelflix.Catalogo.Domain.SeedWork.SearchableRepository;
+using FC.Pixelflix.Catalogo.Domain.Entities;
 
 namespace FC.Pixelflix.Catalogo.IntegrationTests.Infra.Data.EF.Repositories.CategoryRepository;
 
@@ -146,5 +148,43 @@ public class CategoryRepositoryTest
 
         //Then
         dbCategory.Should().BeNull();
+    }
+
+    [Fact(DisplayName = "CategoryRepository Integration Search test with total")]
+    [Trait("Integration/Infra.Data", "CategoryRepository - Repositories")]
+    public async Task givenAValidParams_whenCallsSearch_shouldReturnTotal()
+    {
+        //Given
+        PixelflixCatalogDbContext dbContext = _fixture.CreateDbContext();
+        var categoriesList = _fixture.GetValidCategoryList(15);
+        var aCategoryRepository = new Repository.CategoryRepository(dbContext);
+
+        await dbContext.AddRangeAsync(categoriesList);
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+
+        var searchRequest = new SearchRepositoryRequest(1, 20, "", "", SearchOrder.Asc);
+
+        //When
+        var categoryListResponse = await aCategoryRepository.Search(searchRequest, CancellationToken.None);
+
+        //Then
+        categoryListResponse.Should().NotBeNull();
+        categoryListResponse.Items.Should().NotBeNull();
+        categoryListResponse.CurrentPage.Should().Be(searchRequest.Page);
+        categoryListResponse.PerPage.Should().Be(searchRequest.PerPage);
+        categoryListResponse.Total.Should().Be(categoriesList.Count());
+        categoryListResponse.Items.Should().HaveCount(categoriesList.Count);
+
+        foreach (var category in categoriesList)
+        {
+            var aItem = categoriesList.Find(item => item.Id == category.Id);
+            aItem.Should().NotBeNull();
+
+            category!.Id.Should().Be(aItem.Id);
+            category.Name.Should().Be(aItem.Name);
+            category.Description.Should().Be(aItem.Description);
+            category.IsActive.Should().Be(aItem.IsActive);
+            category.CreatedAt.Should().Be(aItem.CreatedAt);
+        }
     }
 }
