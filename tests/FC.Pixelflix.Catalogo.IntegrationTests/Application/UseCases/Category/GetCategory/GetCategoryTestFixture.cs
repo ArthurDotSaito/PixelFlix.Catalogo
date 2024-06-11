0546711ef61ffcfd629d5e3237d800fa0016ed66
Arthur Saito <arthur.saito@nuria.com.br>
2024-06-10 23:42:32 -0300

    [chore] basic GetCategoryTest Fixture

---
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Application/UseCases/Category/GetCategory/GetCategoryTestFixture.cs
@@ -0,0 +1,15 @@
+﻿using FC.Pixelflix.Catalogo.IntegrationTests.Base;
+using Xunit;
+
+namespace FC.Pixelflix.Catalogo.IntegrationTests.Application.UseCases.Category.GetCategory;
+
+[CollectionDefinition(nameof(GetCategoryTestFixtureCollection))]
+public class GetCategoryTestFixtureCollection: ICollectionFixture<GetCategoryTestFixture>
+{
+    
+}
+
+public class GetCategoryTestFixture: BaseFixture
+{
+    
+}
