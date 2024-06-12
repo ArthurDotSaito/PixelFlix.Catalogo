b36e7cdc815969707236f6e917017b97f2b5991e
Arthur Saito <arthur.saito@nuria.com.br>
2024-06-11 23:09:46 -0300

    [chore] basic config CreateCategoryTest

---
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Application/UseCases/Category/CreateCategory/CreateCategoryTestFixture.cs
@@ -0,0 +1,9 @@
+﻿using FC.Pixelflix.Catalogo.IntegrationTests.Application.UseCases.Category.Common;
+using Xunit;
+
+namespace FC.Pixelflix.Catalogo.IntegrationTests.Application.UseCases.Category.CreateCategory;
+
+[CollectionDefinition(nameof(CreateCategoryTestFixtureCollection))]
+public class CreateCategoryTestFixtureCollection : ICollectionFixture<CreateCategoryTestFixture> { };
+
+public class CreateCategoryTestFixture : CategoryUseCaseBaseFixture { }
---
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Application/UseCases/Category/CreateCategory/CreateCategoryTestIT.cs
@@ -0,0 +1,6 @@
+﻿namespace FC.Pixelflix.Catalogo.IntegrationTests.Application.UseCases.Category.CreateCategory;
+
+public class CreateCategoryTestIT
+{
+    
+}
