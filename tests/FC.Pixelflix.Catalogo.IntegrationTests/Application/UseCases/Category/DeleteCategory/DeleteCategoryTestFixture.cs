b0ce8f98882b623078da356ab277c7e3f43a4b63
Arthur Saito <arthur.saito@nuria.com.br>
2024-06-13 00:28:32 -0300

    [chore] setting up deleteCategory ItTest

---
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Application/UseCases/Category/DeleteCategory/DeleteCategoryTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using FC.Pixelflix.Catalogo.IntegrationTests.Application.UseCases.Category.Common;
+using Xunit;
+
+namespace FC.Pixelflix.Catalogo.IntegrationTests.Application.UseCases.Category.DeleteCategory;
+
+[CollectionDefinition(nameof(DeleteCategoryTestFixtureCollection))]
+public class DeleteCategoryTestFixtureCollection : ICollectionFixture<DeleteCategoryTestFixture> { }
+
+public class DeleteCategoryTestFixture : CategoryUseCaseBaseFixture
+{
+    
+}
