14b27d15566dd6d801c2cc4d1cf2ea3649aed368
Arthur Saito <arthur.saito@nuria.com.br>
2024-06-12 22:14:03 -0300

    [chore] base setting-up UpdateCategoryTEstIT

---
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Application/UseCases/Category/UpdateCategory/UpdateCategoryTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace FC.Pixelflix.Catalogo.IntegrationTests.Application.UseCases.Category.UpdateCategory;
+
+[Collection(nameof(UpdateCategoryTestFixtureCollection))]
+public class UpdateCategoryTest
+{
+    private readonly UpdateCategoryTestFixture _fixture;
+    
+    public UpdateCategoryTest(UpdateCategoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+}
---
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Application/UseCases/Category/UpdateCategory/UpdateCategoryTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using FC.Pixelflix.Catalogo.IntegrationTests.Application.UseCases.Category.Common;
+using Xunit;
+
+namespace FC.Pixelflix.Catalogo.IntegrationTests.Application.UseCases.Category.UpdateCategory;
+
+[CollectionDefinition(nameof(UpdateCategoryTestFixtureCollection))]
+public class UpdateCategoryTestFixtureCollection: ICollectionFixture<UpdateCategoryTestFixture> { }
+
+public class UpdateCategoryTestFixture : CategoryUseCaseBaseFixture
+{
+    
+}
