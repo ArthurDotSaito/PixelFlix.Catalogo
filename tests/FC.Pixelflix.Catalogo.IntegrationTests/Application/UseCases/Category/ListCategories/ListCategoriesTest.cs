8bf0a0ebd6b83c5c1ab64fd822be0569102a5c07
Arthur Saito <arthur.saito@nuria.com.br>
2024-06-17 22:46:40 -0300

    [chore] setting up listCategories Test

---
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Application/UseCases/Category/ListCategories/ListCategoriesTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace FC.Pixelflix.Catalogo.IntegrationTests.Application.UseCases.Category.ListCategories;
+
+[Collection(nameof(ListCategoriesTestFixtureColllection))]
+public class ListCategoriesTest
+{
+    private readonly ListCategoriesTestFixture _fixture;
+    
+    public ListCategoriesTest(ListCategoriesTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+}
---
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Application/UseCases/Category/ListCategories/ListCategoriesTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using FC.Pixelflix.Catalogo.IntegrationTests.Application.UseCases.Category.Common;
+using Xunit;
+
+namespace FC.Pixelflix.Catalogo.IntegrationTests.Application.UseCases.Category.ListCategories;
+
+[CollectionDefinition(nameof(ListCategoriesTestFixtureColllection))]
+public class ListCategoriesTestFixtureColllection : ICollectionFixture<ListCategoriesTestFixture>{ }
+
+public class ListCategoriesTestFixture : CategoryUseCaseBaseFixture
+{
+    
+}
