a56d9ce1ac9809079a3750539e12a181a7c47b33
Arthur Saito <arthur.saito@nuria.com.br>
2024-06-17 22:56:50 -0300

    [test] a list should be empty if empty

--- tests/FC.Pixelflix.Catalogo.IntegrationTests/Application/UseCases/Category/ListCategories/ListCategoriesTest.cs
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Application/UseCases/Category/ListCategories/ListCategoriesTest.cs
@@ -16,8 +16,8 @@ public class ListCategoriesTest
         _fixture = fixture;
     }
     
-    [Fact(DisplayName = "CategoryRepository Integration Search test with total")]
-    [Trait("Integration/Application", "ListCategories - Repositories")]
+    [Fact(DisplayName = "Category List/Search Integration Search test with total")]
+    [Trait("Integration/Application", "ListCategories - UseCases")]
     public async Task givenAValidParams_whenCallsSearch_shouldReturnTotal()
     {
         //Given
@@ -54,5 +54,27 @@ public class ListCategoriesTest
             category.IsActive.Should().Be(aItem.IsActive);
             category.CreatedAt.Should().Be(aItem.CreatedAt);
         }
+    }
+    
+    [Fact(DisplayName = "Category List/Search Integration Search - Empty results should return empty")]
+    [Trait("Integration/Application", "ListCategories - UseCases")]
+    public async Task givenAValidParams_whenCallsSearchAndEmptyTotal_shouldReturnEmpty()
+    {
+        //Given
+        var dbContext = _fixture.CreateDbContext();
+        var aCategoryRepository = new CategoryRepository(dbContext);
+
+        var searchRequest = new ListCategoriesRequest(1, 20);
+        var useCase = new UseCase.ListCategories(aCategoryRepository);
+        //When
+        var categoryListResponse = await useCase.Handle(searchRequest, CancellationToken.None);
+
+        //Then
+        categoryListResponse.Should().NotBeNull();
+        categoryListResponse.Items.Should().NotBeNull();
+        categoryListResponse.Page.Should().Be(searchRequest.Page);
+        categoryListResponse.PerPage.Should().Be(searchRequest.PerPage);
+        categoryListResponse.Total.Should().Be(0);
+        categoryListResponse.Items.Should().HaveCount(0);
     }
 }
