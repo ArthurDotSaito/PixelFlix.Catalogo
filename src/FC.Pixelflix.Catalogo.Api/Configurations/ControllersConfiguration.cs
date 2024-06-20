cf2ceb165a60e78879cb46be3845aea24a36f45c
Arthur Saito <arthur.saito@nuria.com.br>
2024-06-19 22:59:48 -0300

    [chore] Configuring DI Container

---
+++ src/FC.Pixelflix.Catalogo.Api/Configurations/ControllersConfiguration.cs
@@ -0,0 +1,29 @@
+﻿namespace FC.Pixelflix.Catalogo.Api.Configurations;
+
+public static class ControllersConfiguration
+{
+    public static IServiceCollection AddAndConfigureControllers(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddDocumentation();
+        return services;
+    }
+
+    public static WebApplication UseDocumentation(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        return app;
+    }
+    private static IServiceCollection AddDocumentation(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        
+        return services;
+    }
+}
