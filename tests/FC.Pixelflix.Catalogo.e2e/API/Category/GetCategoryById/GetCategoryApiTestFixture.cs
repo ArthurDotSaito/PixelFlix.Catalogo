0e11f9a98d88d752f8f03941cb9f248294a3a939
Arthur Saito <arthur.saito@nuria.com.br>
2024-06-24 22:34:37 -0300

    + Base GetCategoryTest

---
+++ tests/FC.Pixelflix.Catalogo.e2e/API/Category/GetCategoryById/GetCategoryApiTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using FC.Pixelflix.Catalogo.e2e.API.Common;
+using Xunit;
+
+namespace FC.Pixelflix.Catalogo.e2e.API.Category.GetCategoryById;
+
+[CollectionDefinition(nameof(GetCategoryApiTestFixtureCollection))]
+public class GetCategoryApiTestFixtureCollection : ICollectionFixture<GetCategoryApiTestFixture> { }
+
+public class GetCategoryApiTestFixture : CategoryBaseFixture
+{
+    
+}
