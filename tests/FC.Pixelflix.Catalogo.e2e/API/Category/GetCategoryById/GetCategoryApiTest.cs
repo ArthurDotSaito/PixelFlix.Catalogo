4f731b24341cdca7f4b7d0032474a958f760d060
Arthur Saito <arthur.saito@nuria.com.br>
2024-06-25 22:10:44 -0300

    [test] I should not be able to find a category with a invalid id

--- tests/FC.Pixelflix.Catalogo.e2e/API/Category/GetCategoryById/GetCategoryApiTest.cs
+++ tests/FC.Pixelflix.Catalogo.e2e/API/Category/GetCategoryById/GetCategoryApiTest.cs
@@ -2,6 +2,7 @@
 using FC.Pixelflix.Catalogo.Application.UseCases.Category.Common;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Xunit;
 
 namespace FC.Pixelflix.Catalogo.e2e.API.Category.GetCategoryById;
@@ -37,5 +38,27 @@ public class GetCategoryApiTest
         response.Description.Should().Be(aCategory.Description);
         response.IsActive.Should().Be(aCategory.IsActive);
         response.CreatedAt.Should().Be(aCategory.CreatedAt);
+    }
+    
+    [Fact(DisplayName = nameof(GivenAValidId_whenCallsGetCategory_shouldReturnACategory))]
+    [Trait("E2E/Api", "GetCategory - Endpoints")]
+    public async Task GivenAInvalidId_whenCallsGetCategory_shouldReturnNotFoundException()
+    {
+        //given
+        var categoriesList = _fixture.GetValidCategoryList(20);
+        await _fixture.Persistence.InsertList(categoriesList);
+        
+        var anId = Guid.NewGuid();
+        //when
+        var (responseMessage, response) = await _fixture.ApiClient.Get<ProblemDetails>($"/categories/{anId}");
+
+        //then
+        responseMessage.Should().NotBeNull();
+        responseMessage!.StatusCode.Should().Be((HttpStatusCode) StatusCodes.Status404NotFound);
+        response.Should().NotBeNull();
+        response!.Status.Should().Be(StatusCodes.Status404NotFound);
+        response.Title.Should().Be("Not Found");
+        response.Detail.Should().Be($"Category '{anId}' not found.");
+        response.Type.Should().Be("NotFound");
     }
 }
