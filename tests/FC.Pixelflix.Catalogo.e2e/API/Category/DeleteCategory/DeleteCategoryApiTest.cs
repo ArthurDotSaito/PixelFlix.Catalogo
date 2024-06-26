56fcfc7b89f5d543d8d6fefa6e8c0d7a9fdb6ecd
Arthur Saito <arthur.saito@nuria.com.br>
2024-06-25 22:21:29 -0300

    [test] I should be able to delete a category

--- tests/FC.Pixelflix.Catalogo.e2e/API/Category/DeleteCategory/DeleteCategoryApiTest.cs
+++ tests/FC.Pixelflix.Catalogo.e2e/API/Category/DeleteCategory/DeleteCategoryApiTest.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿using System.Net;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Xunit;
 
 namespace FC.Pixelflix.Catalogo.e2e.API.Category.DeleteCategory;
 
@@ -11,6 +14,27 @@ public class DeleteCategoryApiTest
     {
         _fixture = fixture;
     }
-    
+
+    [Fact(DisplayName = nameof(GivenAValidId_whenCallsDeleteById_shouldDeleteThisCategory))]
+    [Trait("E2E/Api", "DeleteCategory - Endpoints")]
+    public async Task GivenAValidId_whenCallsDeleteById_shouldDeleteThisCategory()
+    {
+        //given
+        var categoriesList = _fixture.GetValidCategoryList(20);
+        await _fixture.Persistence.InsertList(categoriesList);
+        
+        var aCategory = categoriesList[10];
+        //when
+        var (responseMessage, response) = await _fixture.ApiClient.Delete<object>($"/categories/{aCategory.Id}");
+
+        //then
+        responseMessage.Should().NotBeNull();
+        responseMessage!.StatusCode.Should().Be((HttpStatusCode) StatusCodes.Status204NoContent);
+        response.Should().BeNull();
+        
+        var deletedCategory = await _fixture.Persistence.GetById(aCategory.Id);
+        
+        deletedCategory.Should().BeNull();
+    }
     
 }
