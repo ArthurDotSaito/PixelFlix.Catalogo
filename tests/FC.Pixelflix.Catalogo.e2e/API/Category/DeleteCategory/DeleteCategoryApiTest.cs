38bca308db80f17d3988c026a3f64e1c8e31b631
Arthur Saito <arthur.saito@nuria.com.br>
2024-06-25 22:16:50 -0300

    [chore] deleteCategoryApi Settings

---
+++ tests/FC.Pixelflix.Catalogo.e2e/API/Category/DeleteCategory/DeleteCategoryApiTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace FC.Pixelflix.Catalogo.e2e.API.Category.DeleteCategory;
+
+[Collection(nameof(DeleteCategoryApiTestCollection))]
+public class DeleteCategoryApiTest
+{
+    private readonly DeleteCategoryApiTestFixture _fixture;
+    
+    public DeleteCategoryApiTest(DeleteCategoryApiTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+    
+    
+}
---
+++ tests/FC.Pixelflix.Catalogo.e2e/API/Category/DeleteCategory/DeleteCategoryApiTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using FC.Pixelflix.Catalogo.e2e.API.Common;
+using Xunit;
+
+namespace FC.Pixelflix.Catalogo.e2e.API.Category.DeleteCategory;
+
+[CollectionDefinition(nameof(DeleteCategoryApiTestCollection))]
+public class DeleteCategoryApiTestCollection: ICollectionFixture<DeleteCategoryApiTestFixture>{}
+
+public class DeleteCategoryApiTestFixture : CategoryBaseFixture
+{
+    
+}
