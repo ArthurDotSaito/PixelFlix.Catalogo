180c0ec7b9afd10538f15a1748ada9872efacdc5
Arthur Saito <arthur.saito@nuria.com.br>
2024-06-26 21:36:17 -0300

    [test] I should be able to updateACategory with only name

--- tests/FC.Pixelflix.Catalogo.e2e/API/Category/UpdateCategory/UpdateCategoryApiTest.cs
+++ tests/FC.Pixelflix.Catalogo.e2e/API/Category/UpdateCategory/UpdateCategoryApiTest.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using FC.Pixelflix.Catalogo.Application.UseCases.Category.Common;
+using FC.Pixelflix.Catalogo.Application.UseCases.Category.UpdateCategory;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Xunit;
@@ -45,5 +46,39 @@ public class UpdateCategoryApiTest
         categoryInDatabase.Name.Should().Be(request.Name);
         categoryInDatabase.Description.Should().Be(request.Description);
         categoryInDatabase.IsActive.Should().Be((bool)request.IsActive);
+    }
+    
+    [Fact(DisplayName = nameof(GivenAValidId_whenCallsUpdateCategoryWithNameOnly_shouldReturnACategory))]
+    [Trait("E2E/Api", "UpdateCategory - Endpoints")]
+    public async void GivenAValidId_whenCallsUpdateCategoryWithNameOnly_shouldReturnACategory()
+    {
+        //given
+        var categoriesList = _fixture.GetValidCategoryList(20);
+        await _fixture.Persistence.InsertList(categoriesList);
+        
+        var aCategory = categoriesList[10];
+        var request = new UpdateCategoryRequest(aCategory.Id, _fixture.GetValidCategoryName())
+        {
+            IsActive = aCategory.IsActive
+        };
+        //when
+        var (responseMessage, response) = await _fixture.ApiClient.Put<CategoryModelResponse>($"/categories/{aCategory.Id}", request);
+        
+        //then
+        responseMessage.Should().NotBeNull();
+        responseMessage!.StatusCode.Should().Be(HttpStatusCode.OK);
+        response.Should().NotBeNull();
+        response!.Id.Should().Be(aCategory.Id);
+        response.Name.Should().Be(request.Name);
+        response.Description.Should().Be(aCategory.Description);
+        response.IsActive.Should().Be(aCategory.IsActive);
+
+        var categoryInDatabase = await _fixture.Persistence.GetById(aCategory.Id);
+        
+        categoryInDatabase.Should().NotBeNull();
+        categoryInDatabase!.Id.Should().Be(request.Id);
+        categoryInDatabase.Name.Should().Be(request.Name);
+        categoryInDatabase.Description.Should().Be(aCategory.Description);
+        categoryInDatabase.IsActive.Should().Be(aCategory.IsActive);
     }
 }
