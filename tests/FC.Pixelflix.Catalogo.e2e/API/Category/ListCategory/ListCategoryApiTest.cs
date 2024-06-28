5ff838e327c5b77b9a4c5eae858873ee4b09e9bc
Arthur Saito <arthur.saito@nuria.com.br>
2024-06-27 23:14:29 -0300

    [chore] ListCategoryTestFixture Setting

---
+++ tests/FC.Pixelflix.Catalogo.e2e/API/Category/ListCategory/ListCategoryApiTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace FC.Pixelflix.Catalogo.e2e.API.Category.ListCategory;
+
+[Collection(nameof(ListCategoryApiTestFixtureCollection))]
+public class ListCategoryApiTest
+{
+    private readonly ListCategoryApiTestFixture _fixture;
+    
+    public ListCategoryApiTest(ListCategoryApiTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+    
+}
