657d6790312c6cc6194fcf208e64de2977078030
Arthur Saito <arthur.saito@nuria.com.br>
2024-07-10 23:26:27 -0300

    + e2e database context

--- tests/FC.Pixelflix.Catalogo.e2e/Base/CustomWebApplicationFactory.cs
+++ tests/FC.Pixelflix.Catalogo.e2e/Base/CustomWebApplicationFactory.cs
@@ -10,9 +10,22 @@ public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStar
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.UseEnvironment("e2e-test");
         builder.ConfigureServices(services =>
         {
-            var databaseOptions = services.FirstOrDefault(s => s.ServiceType == typeof(DbContextOptions<PixelflixCatalogDbContext>));
+            var serviceProvider = services.BuildServiceProvider();
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetService<PixelflixCatalogDbContext>();
+                if (dbContext is null) throw new ArgumentNullException(nameof(dbContext));
+                
+                dbContext.Database.EnsureDeleted();
+                dbContext.Database.EnsureCreated();
+            }
+            
+            // ---- InMemoryDatabase ---- //
+            
+            /*var databaseOptions = services.FirstOrDefault(s => s.ServiceType == typeof(DbContextOptions<PixelflixCatalogDbContext>));
             if (databaseOptions is not null)
             {
                 services.Remove(databaseOptions);
@@ -21,7 +34,7 @@ public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStar
             services.AddDbContext<PixelflixCatalogDbContext>(options =>
             {
                 options.UseInMemoryDatabase("e2e-tests-db");
-            });
+            });*/
         });
         
         base.ConfigureWebHost(builder);
