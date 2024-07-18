0aa0f5ba6f5d8e064004da0407ef5e660d4adad4
Arthur Saito <arthur.saito@nuria.com.br>
2024-07-17 22:09:28 -0300

    Extension to trim DateTime

---
+++ tests/FC.Pixelflix.Catalogo.e2e/Extensions/DateTime/DateTimeExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace FC.Pixelflix.Catalogo.e2e.Extensions.DateTime;
+using SystemDateTime = System.DateTime;
+
+internal static class DateTimeExtensions
+{
+    public static SystemDateTime TrimMilliseconds(this SystemDateTime dateTime)
+    {
+        return new SystemDateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, dateTime.Second, dateTime.Kind);
+    }
+}
