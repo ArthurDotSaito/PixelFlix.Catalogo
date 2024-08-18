ad1ad57ab8f477acdf7dab4abb91a9d1fb34f61a
Arthur Saito <arthur.saito@nuria.com.br>
2024-08-18 17:59:32 -0300

    ++ API Extension && Policy

---
+++ src/FC.Pixelflix.Catalogo.Api/Configurations/Policies/JsonSnakeCasePolicy.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+using FC.Pixelflix.Catalogo.Api.Extensions.String;
+
+namespace FC.Pixelflix.Catalogo.Api.Configurations.Policies;
+
+public class JsonSnakeCasePolicy : JsonNamingPolicy
+{
+    public override string ConvertName(string name)
+    {
+        return name.ToSnakeCase();
+    }
+}
