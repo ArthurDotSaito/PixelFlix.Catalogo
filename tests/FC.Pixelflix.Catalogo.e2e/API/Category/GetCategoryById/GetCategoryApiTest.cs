43179bdd618de7c800a785aab33ba918207016e6
Arthur Saito <arthur.saito@nuria.com.br>
2024-08-21 22:57:54 -0300

    first test with Data prop

--- tests/FC.Pixelflix.Catalogo.e2e/API/Category/GetCategoryById/GetCategoryApiTest.cs
+++ tests/FC.Pixelflix.Catalogo.e2e/API/Category/GetCategoryById/GetCategoryApiTest.cs
@@ -8,6 +8,11 @@ using Xunit;
 
 namespace FC.Pixelflix.Catalogo.e2e.API.Category.GetCategoryById;
 
+class GetCategoryResponse
+{
+    public CategoryModelResponse Data { get; }
+}
+
 [Collection(nameof(GetCategoryApiTestFixtureCollection))]
 public class GetCategoryApiTest : IDisposable
 {
@@ -28,17 +33,18 @@ public class GetCategoryApiTest : IDisposable
         
         var aCategory = categoriesList[10];
         //when
-        var (responseMessage, response) = await _fixture.ApiClient.Get<CategoryModelResponse>($"/categories/{aCategory.Id}");
+        var (responseMessage, response) = await _fixture.ApiClient.Get<GetCategoryResponse>($"/categories/{aCategory.Id}");
 
         //then
         responseMessage.Should().NotBeNull();
         responseMessage!.StatusCode.Should().Be((HttpStatusCode) StatusCodes.Status200OK);
         response.Should().NotBeNull();
-        response!.Id.Should().Be(aCategory.Id);
-        response.Name.Should().Be(aCategory.Name);
-        response.Description.Should().Be(aCategory.Description);
-        response.IsActive.Should().Be(aCategory.IsActive);
-        response.CreatedAt.TrimMilliseconds().Should().Be(aCategory.CreatedAt.TrimMilliseconds());
+        response!.Data.Should().NotBeNull();
+        response!.Data.Id.Should().Be(aCategory.Id);
+        response.Data.Name.Should().Be(aCategory.Name);
+        response.Data.Description.Should().Be(aCategory.Description);
+        response.Data.IsActive.Should().Be(aCategory.IsActive);
+        response.Data.CreatedAt.TrimMilliseconds().Should().Be(aCategory.CreatedAt.TrimMilliseconds());
     }
     
     [Fact(DisplayName = nameof(GivenAValidId_whenCallsGetCategory_shouldReturnACategory))]
