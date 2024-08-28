2a8251c29f98793c7dd0751e2229bf017e293259
Arthur Saito <arthur.saito@nuria.com.br>
2024-08-27 23:45:18 -0300

    ++ Activate Test

--- tests/FC.PixelFlix.Catalogo.UnitTests/Domain/Entities/Genre/GenreTest.cs
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Domain/Entities/Genre/GenreTest.cs
@@ -19,7 +19,7 @@ public class GenreTest
    [Trait("Domain", "Genre - Aggregates")]
    public void GivenAGenreNewInstance_WhenEverythingIsValid_ShouldBeInstantiateAGenre()
    {
-      var expectedName = "Horror";
+      var expectedName = _fixture.GetValidName();
       var dateTimeBefore = DateTime.Now;
       var dateTimeAfter = DateTime.Now.AddSeconds(1);
       
@@ -39,7 +39,7 @@ public class GenreTest
    [InlineData(false)]
    public void GivenAGenreNewInstance_WhenIsActiveTrue_ShouldBeInstantiateAGenre(bool isActive)
    {
-      var expectedName = "Horror";
+      var expectedName = _fixture.GetValidName();
       var dateTimeBefore = DateTime.Now;
       var dateTimeAfter = DateTime.Now.AddSeconds(1);
       
@@ -51,6 +51,23 @@ public class GenreTest
       genre.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
       genre.CreatedAt.Should().BeAfter(dateTimeBefore);
       genre.CreatedAt.Should().BeBefore(dateTimeAfter);
+   }
+   
+   [Theory(DisplayName = nameof(GivenAGenreNewInstance_WhenIsActiveTrue_ShouldBeInstantiateAGenre))]
+   [Trait("Domain", "Genre - Aggregates")]
+   [InlineData(true)]
+   [InlineData(false)]
+   public void GivenAGenre_WhenCallActivate_ShouldIsActiveTrue(bool isActive)
+   {
+      var expectedName = _fixture.GetValidName();
+      var genre = new GenreDomain(expectedName, isActive);
+
+      genre.Activate();
+
+      genre.Should().NotBeNull();
+      genre.Name.Should().Be(expectedName);
+      genre.IsActive.Should().Be(isActive);
+      genre.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
    }
    
 }
