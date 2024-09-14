1e8634b8c24a6eed95e0cc883c3c3c523511961e
Arthur Saito <arthur.saito@nuria.com.br>
2024-09-13 21:23:10 -0300

    [feat] base updateGenreTest

---
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/Genre/UpdateGenre/UpdateGenreTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace FC.PixelFlix.Catalogo.UnitTests.Application.Genre.UpdateGenre;
+
+[Collection(nameof(UpdateGenreTestFixture))]
+public class UpdateGenreTest
+{
+    public UpdateGenreTestFixture _fixture;
+
+    public UpdateGenreTest(UpdateGenreTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+    
+}
---
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/Genre/UpdateGenre/UpdateGenreTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using FC.PixelFlix.Catalogo.UnitTests.Application.Genre.Common;
+using Xunit;
+
+namespace FC.PixelFlix.Catalogo.UnitTests.Application.Genre.UpdateGenre;
+
+[CollectionDefinition(nameof(UpdateGenreTestFixture))]
+public class CreateGenreTestFixtureCollection(): ICollectionFixture<UpdateGenreTestFixture> { }
+
+public class UpdateGenreTestFixture : GenreUseCasesBaseFixture
+{
+    
+}
