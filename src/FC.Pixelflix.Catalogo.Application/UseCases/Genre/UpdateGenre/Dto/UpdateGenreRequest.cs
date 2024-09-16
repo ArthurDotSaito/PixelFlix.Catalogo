48ec961e3be5e1069346f263d7387739e252eeb3
Arthur Saito <arthur.saito@nuria.com.br>
2024-09-15 23:48:26 -0300

    [feat] should be able to updateAGenre with one or more categories

--- src/FC.Pixelflix.Catalogo.Application/UseCases/Genre/UpdateGenre/Dto/UpdateGenreRequest.cs
+++ src/FC.Pixelflix.Catalogo.Application/UseCases/Genre/UpdateGenre/Dto/UpdateGenreRequest.cs
@@ -8,11 +8,13 @@ public class UpdateGenreRequest: IRequest<GenreModelResponse>
     public Guid Id { get; set; }
     public string Name { get; set; }
     public bool? IsActive { get; set; } = null;
+    public List<Guid>? CategoryIds { get; set; }
 
-    public UpdateGenreRequest(Guid id, string name, bool? isActive = null)
+    public UpdateGenreRequest(Guid id, string name, bool? isActive = null, List<Guid> categoryIds = null)
     {
         Id = id;
         Name = name;
         IsActive = isActive;
+        CategoryIds = categoryIds;
     }
 }
