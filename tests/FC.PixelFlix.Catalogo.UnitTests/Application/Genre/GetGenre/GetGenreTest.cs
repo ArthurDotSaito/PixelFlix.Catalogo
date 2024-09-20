6e5204e46dcdcacac11f55d98fa8556f2250e3c9
Arthur Saito <arthur.saito@nuria.com.br>
2024-09-19 21:36:13 -0300

    [test] should thrown a exception when 404

--- tests/FC.PixelFlix.Catalogo.UnitTests/Application/Genre/GetGenre/GetGenreTest.cs
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/Genre/GetGenre/GetGenreTest.cs
@@ -1,4 +1,5 @@
-﻿using FC.Pixelflix.Catalogo.Application.UseCases.Genre.GetGenre.Dto;
+﻿using FC.Pixelflix.Catalogo.Application.Exceptions;
+using FC.Pixelflix.Catalogo.Application.UseCases.Genre.GetGenre.Dto;
 using FluentAssertions;
 using UseCase = FC.Pixelflix.Catalogo.Application.UseCases.Genre.GetGenre;
 using Moq;
@@ -47,6 +48,29 @@ public class GetGenreTest
         }
         
         genreRepositoryMock.Verify(x=>x.Get(It.Is<Guid>(e=>e== aGenre.Id),
+            It.IsAny<CancellationToken>()), Times.Once);
+    }
+    
+    [Fact(DisplayName = nameof(GivenGetCommand_whenThereIsNoGenre_shouldThrowsNotFound))]
+    [Trait("Application", "GetGenre - Use Cases")]
+    public async Task GivenGetCommand_whenThereIsNoGenre_shouldThrowsNotFound()
+    {
+        var genreRepositoryMock = _fixture.GetGenreRepositoryMock();
+    
+        var aGuid = Guid.NewGuid();
+        
+        genreRepositoryMock.Setup(x => x.Get(It.Is<Guid>(id => id == aGuid), It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new NotFoundException($"Genre {aGuid} not found"));
+        
+        var useCase = new UseCase.GetGenre(genreRepositoryMock.Object);
+        
+        var input = new GetGenreRequest(aGuid);
+        
+        var action = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await action.Should().ThrowAsync<NotFoundException>().WithMessage($"Genre {aGuid} not found");
+        
+        genreRepositoryMock.Verify(x=>x.Get(It.Is<Guid>(e=>e== aGuid),
             It.IsAny<CancellationToken>()), Times.Once);
     }
 }
