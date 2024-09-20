7c993f08a9e1e3f98274e38791e97e3abe4f1c19
Arthur Saito <arthur.saito@nuria.com.br>
2024-09-19 21:24:35 -0300

    [test] should be able to get a genre

--- tests/FC.PixelFlix.Catalogo.UnitTests/Application/Genre/GetGenre/GetGenreTest.cs
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/Genre/GetGenre/GetGenreTest.cs
@@ -1,5 +1,10 @@
-﻿namespace FC.PixelFlix.Catalogo.UnitTests.Application.Genre.GetGenre;
+﻿using FC.Pixelflix.Catalogo.Application.UseCases.Genre.UpdateGenre.Dto;
+using Moq;
+using Xunit;
 
+namespace FC.PixelFlix.Catalogo.UnitTests.Application.Genre.GetGenre;
+
+[Collection(nameof(GetGenreTestFixture))]
 public class GetGenreTest
 {
     private readonly GetGenreTestFixture _fixture;
@@ -7,5 +12,36 @@ public class GetGenreTest
     public GetGenreTest(GetGenreTestFixture fixture)
     {
         _fixture = fixture;
+    }
+    
+    [Fact(DisplayName = nameof(GivenAValidCommand_whenCallsGetGenre_shouldGetAGenre))]
+    [Trait("Application", "GetGenre - Use Cases")]
+    public async Task GivenAValidCommand_whenCallsGetGenre_shouldGetAGenre()
+    {
+        var genreRepositoryMock = _fixture.GetGenreRepositoryMock();
+
+        var someCategories = _fixture.GenerateRandomCategoryIds(10);
+        var aGenre = _fixture.GetValidGenreWithCategories(categoryIds: someCategories);
+
+        genreRepositoryMock.Setup(x => x.Get(It.Is<Guid>(id=>id == aGenre.Id), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(aGenre);
+        
+        var useCase = new UseCase.GetGenre(genreRepositoryMock.Object);
+        
+        var input = new GetGenreRequest(aGenre.Id);
+        
+        var output = await useCase.Handle(input, CancellationToken.None);
+        
+        output.Should().NotBeNull();
+        output.Id.Should().Be(aGenre.Id);
+        output.Name.Should().Be(aGenre.Name);
+        output.IsActive.Should().Be(aGenre.IsActive);
+        output.CreatedAt.Should().BeSameDateAs(aGenre.CreatedAt);
+        output.Categories.Should().HaveCount(someCategories.Count);
+        
+        someCategories.ForEach(expectedId => output.Categories.Should().Contain(expectedId));
+        
+        genreRepositoryMock.Verify(x=>x.Update(It.Is<GenreDomain>(e=>e.Id == aGenre.Id),
+            It.IsAny<CancellationToken>()), Times.Once);
     }
 }
