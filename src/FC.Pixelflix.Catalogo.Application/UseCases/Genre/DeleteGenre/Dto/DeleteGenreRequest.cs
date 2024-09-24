6ca3dc8430f7b962b176342e9a625eedb727d6d0
Arthur Saito <arthur.saito@nuria.com.br>
2024-09-23 22:46:49 -0300

    [feat] should be able to delete a genre

---
+++ src/FC.Pixelflix.Catalogo.Application/UseCases/Genre/DeleteGenre/Dto/DeleteGenreRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace FC.Pixelflix.Catalogo.Application.UseCases.Genre.DeleteGenre.Dto;
+
+public class DeleteGenreRequest : IRequest
+{
+    public Guid Id { get; set; }
+
+    public DeleteGenreRequest(Guid id)
+    {
+        Id = id;
+    }
+}
--- tests/FC.PixelFlix.Catalogo.UnitTests/Application/Genre/DeleteGenre/DeleteGenreTest.cs
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/Genre/DeleteGenre/DeleteGenreTest.cs
@@ -1,8 +1,7 @@
-﻿using FC.Pixelflix.Catalogo.Application.UseCases.Genre.UpdateGenre.Dto;
-using FluentAssertions;
+﻿using FC.Pixelflix.Catalogo.Application.UseCases.Genre.DeleteGenre.Dto;
 using Moq;
 using Xunit;
-using UseCase = FC.Pixelflix.Catalogo.Application.UseCases.Genre.UpdateGenre;
+using UseCase = FC.Pixelflix.Catalogo.Application.UseCases.Genre.DeleteGenre;
 using GenreDomain = FC.Pixelflix.Catalogo.Domain.Entities.Genre;
 
 namespace FC.PixelFlix.Catalogo.UnitTests.Application.Genre.DeleteGenre;
@@ -17,22 +16,19 @@ public class DeleteGenreTest
         _fixture = fixture;
     }
     
-    [Fact(DisplayName = nameof(GivenAValidCommand_whenCallsUpdateGenre_shouldUpdate))]
-    [Trait("Application", "UpdateGenre - Use Cases")]
-    public async Task GivenAValidCommand_whenCallsUpdateGenre_shouldUpdate()
+    [Fact(DisplayName = nameof(GivenAValidCommand_whenCallsDeleteGenre_shouldUpdate))]
+    [Trait("Application", "DeleteGenre - Use Cases")]
+    public async Task GivenAValidCommand_whenCallsDeleteGenre_shouldUpdate()
     {
         var genreRepositoryMock = _fixture.GetGenreRepositoryMock();
         var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
-        var categoryRepositoryMock = _fixture.GetCategoryRepositoryMock();
         
         var aGenre = _fixture.GetValidGenre();
-        var newName = _fixture.GetValidGenreName();
-        var newIsActive = !aGenre.IsActive;
 
         genreRepositoryMock.Setup(x => x.Get(It.Is<Guid>(id=>id == aGenre.Id), It.IsAny<CancellationToken>()))
             .ReturnsAsync(aGenre);
         
-        var useCase = new UseCase.DeleteGenre(categoryRepositoryMock.Object, genreRepositoryMock.Object, unitOfWorkMock.Object);
+        var useCase = new UseCase.DeleteGenre(genreRepositoryMock.Object, unitOfWorkMock.Object);
         
         var input = new DeleteGenreRequest(aGenre.Id);
         
