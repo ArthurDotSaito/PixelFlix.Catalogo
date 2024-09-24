98d9d37b364f9eb05d56655fbf2bca274ed4e791
Arthur Saito <arthur.saito@nuria.com.br>
2024-09-23 22:18:43 -0300

    [chore] baseTest Genre

---
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/Genre/DeleteGenre/DeleteGenreTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace FC.PixelFlix.Catalogo.UnitTests.Application.Genre.DeleteGenre;
+
+[CollectionDefinition(nameof(DeleteGenreTestFixture))]
+public class DeleteGenreTest
+{
+    private readonly DeleteGenreTestFixture _fixture;
+
+    public DeleteGenreTest(DeleteGenreTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+}
