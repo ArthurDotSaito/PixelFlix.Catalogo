1417d1810ff33629a15ae44648ad0901f77b5d65
Arthur Saito <arthur.saito@nuria.com.br>
2024-09-30 23:45:47 -0300

    base class ListGenreTest

--- tests/FC.PixelFlix.Catalogo.UnitTests/Application/Genre/ListGenre/ListGenreTest.cs
+++ tests/FC.PixelFlix.Catalogo.UnitTests/Application/Genre/ListGenre/ListGenreTest.cs
@@ -5,5 +5,10 @@ namespace FC.PixelFlix.Catalogo.UnitTests.Application.Genre.ListGenre;
 [Collection(nameof(ListGenreTestFixture))]
 public class ListGenreTest
 {
-    
+    private readonly ListGenreTestFixture _fixture;
+
+    public ListGenreTest(ListGenreTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
 }
