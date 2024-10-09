79cb4b2763aad720c9f5d69899c9b554ca171b39
Arthur Saito <arthur.saito@nuria.com.br>
2024-10-08 23:48:39 -0300

    ++ some genre fixtures

--- tests/FC.Pixelflix.Catalogo.IntegrationTests/Infra.Data.EF/Repositories/GenreRepository/GenreRepositoryTest.cs
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Infra.Data.EF/Repositories/GenreRepository/GenreRepositoryTest.cs
@@ -1,4 +1,8 @@
-﻿namespace FC.Pixelflix.Catalogo.IntegrationTests.Infra.Data.EF.Repositories.GenreRepository;
+﻿using FC.Pixelflix.Catalogo.Infra.Data.EF;
+using FluentAssertions;
+using Xunit;
+
+namespace FC.Pixelflix.Catalogo.IntegrationTests.Infra.Data.EF.Repositories.GenreRepository;
 
 public class GenreRepositoryTest
 {
@@ -7,5 +11,30 @@ public class GenreRepositoryTest
     public GenreRepositoryTest(GenreRepositoryTestFixture fixture)
     {
         _fixture = fixture;
+    }
+    
+    [Fact(DisplayName = "CategoryRepository Integration INSERT test")]
+    [Trait("Integration/Infra.Data", "GenreRepository - Repositories")]
+    public async Task givenAValidGenre_whenCallsInsert_shouldPersistAGenre()
+    {
+        //Given
+        PixelflixCatalogDbContext dbContext = _fixture.CreateDbContext();
+        var aCategory = _fixture.GetValidCategory();
+
+        var aCategoryRepository = new Repository.CategoryRepository(dbContext);
+
+        //When
+        await aCategoryRepository.Insert(aCategory, CancellationToken.None);
+        await dbContext.SaveChangesAsync();
+
+        PixelflixCatalogDbContext aSecondContext = _fixture.CreateDbContext(true);
+        var dbCategory = await aSecondContext.Categories.FindAsync(aCategory.Id);
+
+        //Then
+        dbCategory.Should().NotBeNull();
+        dbCategory!.Name.Should().Be(aCategory.Name);
+        dbCategory.Description.Should().Be(aCategory.Description);
+        dbCategory.IsActive.Should().Be(aCategory.IsActive);
+        dbCategory.CreatedAt.Should().Be(aCategory.CreatedAt);
     }
 }
--- tests/FC.Pixelflix.Catalogo.IntegrationTests/Infra.Data.EF/Repositories/GenreRepository/GenreRepositoryTestFixture.cs
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Infra.Data.EF/Repositories/GenreRepository/GenreRepositoryTestFixture.cs
@@ -1,5 +1,6 @@
 ﻿using FC.Pixelflix.Catalogo.IntegrationTests.Base;
 using Xunit;
+using DomainGenre = FC.Pixelflix.Catalogo.Domain.Entities.Genre;
 
 namespace FC.Pixelflix.Catalogo.IntegrationTests.Infra.Data.EF.Repositories.GenreRepository;
 
@@ -8,5 +9,36 @@ public class GenreRepositoryTestFixtureCollection : ICollectionFixture<GenreRepo
 
 public class GenreRepositoryTestFixture: BaseFixture
 {
+    public string GetValidGenreName()
+    {
+        return Faker.Commerce.Categories(1)[0];
+    }
     
+    public DomainGenre GetValidGenre()
+    {
+        var aName = GetValidGenreName();
+        var isActive = GetRandomIsActive();
+        return new DomainGenre(aName, isActive);
+    }
+    public DomainGenre GetValidGenre(bool? isActive = null)
+    {
+        var aName = GetValidGenreName();
+        var active = isActive ?? GetRandomIsActive();
+        return new DomainGenre(aName, active);
+    }
+    
+    public DomainGenre GetValidGenreWithCategories(bool? isActive = null , List<Guid> categoryIds = null)
+    {
+        var aName = GetValidGenreName();
+        var active = isActive ?? GetRandomIsActive();
+        
+        var genre = new DomainGenre(aName, active);
+        categoryIds?.ForEach(categoryId => genre.AddCategory(categoryId));
+        return genre;
+    }
+    
+    public bool GetRandomIsActive()
+    {
+        return new Random().NextDouble() < 0.5;
+    }
 }
