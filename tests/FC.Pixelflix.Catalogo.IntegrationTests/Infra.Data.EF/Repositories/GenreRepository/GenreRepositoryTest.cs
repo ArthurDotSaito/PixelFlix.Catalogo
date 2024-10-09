df2e4efc4fbb28ffe77959b3fd022d5ceda97f5e
Arthur Saito <arthur.saito@nuria.com.br>
2024-10-08 23:45:34 -0300

    basic setup GenreRepositoryFixture

---
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Infra.Data.EF/Repositories/GenreRepository/GenreRepositoryTest.cs
@@ -0,0 +1,11 @@
+﻿namespace FC.Pixelflix.Catalogo.IntegrationTests.Infra.Data.EF.Repositories.GenreRepository;
+
+public class GenreRepositoryTest
+{
+    private readonly GenreRepositoryTestFixture _fixture;
+
+    public GenreRepositoryTest(GenreRepositoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+}
---
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Infra.Data.EF/Repositories/GenreRepository/GenreRepositoryTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using FC.Pixelflix.Catalogo.IntegrationTests.Base;
+using Xunit;
+
+namespace FC.Pixelflix.Catalogo.IntegrationTests.Infra.Data.EF.Repositories.GenreRepository;
+
+[CollectionDefinition(nameof(GenreRepositoryTestFixtureCollection))]
+public class GenreRepositoryTestFixtureCollection : ICollectionFixture<GenreRepositoryTestFixture> { }
+
+public class GenreRepositoryTestFixture: BaseFixture
+{
+    
+}
