a968979fd751864b4cc4c37cc2d4f2db9911b95d
Arthur Saito <arthur.saito@nuria.com.br>
2024-11-06 23:32:28 -0300

    base getGenreTest

---
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Application/UseCases/Genre/GetGenre/GetGenreTest.cs
@@ -0,0 +1,17 @@
+﻿using FC.Pixelflix.Catalogo.IntegrationTests.Application.UseCases.Genre.Common;
+using Xunit;
+
+namespace FC.Pixelflix.Catalogo.IntegrationTests.Application.UseCases.Genre.GetGenre;
+
+[Collection(nameof(GetGenreTestFixtureCollection))]
+public class GetGenreTest
+{
+    private readonly GetGenreTestFixture _fixture;
+    
+    public GetGenreTest(GetGenreTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+    
+    
+}
---
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Application/UseCases/Genre/GetGenre/GetGenreTestFixture.cs
@@ -0,0 +1,13 @@
+﻿using FC.Pixelflix.Catalogo.IntegrationTests.Application.UseCases.Genre.Common;
+using Xunit;
+
+namespace FC.Pixelflix.Catalogo.IntegrationTests.Application.UseCases.Genre.GetGenre;
+
+
+[CollectionDefinition(nameof(GetGenreTestFixtureCollection))]
+public class GetGenreTestFixtureCollection: ICollectionFixture<GetGenreTestFixture> { }
+
+public class GetGenreTestFixture : GenreUseCaseBaseFixture
+{
+    
+}
