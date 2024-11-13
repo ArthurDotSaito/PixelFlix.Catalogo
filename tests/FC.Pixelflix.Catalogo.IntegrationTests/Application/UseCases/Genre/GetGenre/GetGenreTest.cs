8fb1756949da2746372797b87c1342fef5b7eaf8
Arthur Saito <arthur.saito@nuria.com.br>
2024-11-12 23:48:14 -0300

    [test] should thrown a Exception when NotFound

--- tests/FC.Pixelflix.Catalogo.IntegrationTests/Application/UseCases/Genre/GetGenre/GetGenreTest.cs
+++ tests/FC.Pixelflix.Catalogo.IntegrationTests/Application/UseCases/Genre/GetGenre/GetGenreTest.cs
@@ -1,4 +1,5 @@
-﻿using FC.Pixelflix.Catalogo.Application.UseCases.Genre.GetGenre.Dto;
+﻿using FC.Pixelflix.Catalogo.Application.Exceptions;
+using FC.Pixelflix.Catalogo.Application.UseCases.Genre.GetGenre.Dto;
 using FC.Pixelflix.Catalogo.Infra.Data.EF.Repositories;
 using FluentAssertions;
 using Xunit;
@@ -20,12 +21,12 @@ public class GetGenreTest
     [Trait("Integration/Application", "GetGenre - useCases")]
     public async Task GivenAValidId_whenCallsGetGenre_shouldReturnACategory()
     {
-        var dbcontext = _fixture.CreateDbContext();
+        var dbContext = _fixture.CreateDbContext();
         
         var aGenreList = _fixture.GetValidGenreList();
         var expectedGenre = aGenreList[5];
-        await dbcontext.Genres.AddRangeAsync(aGenreList);
-        await dbcontext.SaveChangesAsync();
+        await dbContext.Genres.AddRangeAsync(aGenreList);
+        await dbContext.SaveChangesAsync();
 
         var genreRepository = new GenreRepository(_fixture.CreateDbContext(true));
         var request = new GetGenreRequest(expectedGenre.Id);
@@ -38,5 +39,26 @@ public class GetGenreTest
         response.Name.Should().Be(expectedGenre.Name);
         response.IsActive.Should().Be(expectedGenre.IsActive);
         response.CreatedAt.Should().Be(expectedGenre.CreatedAt);
+    }
+    
+    [Fact(DisplayName = nameof(GivenAValidId_whenThereIsNoId_shouldThrownNotFound))]
+    [Trait("Integration/Application", "GetGenre - useCases")]
+    public async Task GivenAValidId_whenThereIsNoId_shouldThrownNotFound()
+    {
+        var dbContext = _fixture.CreateDbContext();
+        var randomId = Guid.NewGuid();
+        
+        var aGenreList = _fixture.GetValidGenreList();
+        var expectedGenre = aGenreList[5];
+        await dbContext.Genres.AddRangeAsync(aGenreList);
+        await dbContext.SaveChangesAsync();
+
+        var genreRepository = new GenreRepository(_fixture.CreateDbContext(true));
+        var request = new GetGenreRequest(randomId);
+        var useCase = new UseCase.GetGenre(genreRepository);
+        
+        var output = async() => await useCase.Handle(request, CancellationToken.None);
+
+        await output.Should().ThrowAsync<NotFoundException>().WithMessage($"Genre '{randomId}' was not found");
     }
 }
